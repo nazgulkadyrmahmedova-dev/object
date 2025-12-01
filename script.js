b3a2a17743d82990f23ef0a600b1a0d01c4846ba
nazgulkadyrmahmedova-dev <nazgulkadyrmahmedova@gmail.com>
2025-12-01 20:19:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// let student = {
+//     name:'Nazgul',
+//     age: 43,
+//     hobby: ['карагат айдоо,мал багуу,гул айдоо'],
+
+// }
+// student.age = 44
+// console.log(student)
+
+
+// let useName=prompt('Атынызды жазгыла')
+// let usePassword=prompt("Пароль жазыныз")
+// let user={}
+// user. name= useName
+// user. Password=usePassword
+// console.log(user)
+
+
+// let phon={
+//     name:'redmi',
+//     year: 2023
+// }
+// console.log(phon)
+
+let student={
+    name: 'Nazgul',
+    age: 7,
+    class: 1,
+    hobby: ['китеп окуу, фантик чогултуу'],
+}
+student.age=43
+student.class=Worker
+student.hobby=['карагат, гул, жашылча-жемиш айдоо.']
+console.log(student)
